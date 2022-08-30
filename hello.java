de46cc4662803a6d5a98905a8801b215ad979ca1
elsaninan <61472786+elsaninan@users.noreply.github.com>
2022-08-30 09:24:45 +0000

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,21 @@
+package p1;
+import java.util.*;
+
+public class hello {
+
+
+public static void main(String args[])
+{int x,y,z;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter x value ");
+x=s.nextInt();
+System.out.println("Enter y value ");
+y=s.nextInt();
+z=x+y;
+
+System.out.println("Sum is  "+z);
+
+}
+
+
+}
